e921602fee2f89b3d6c207cf9af9a46a7f5bbb2a
Albert Bertram <bertrama@umich.edu>
2016-03-09 14:05:18 -0500

    Intial setup.

---
+++ lib/umlaut-three-sixty-link.rb
@@ -0,0 +1,8 @@
+# Copyright (c) 2016, Regents of the University of Michigan.
+# All rights reserved. See LICENSE.txt for details.
+
+require "umlaut-three-sixty-link/version"
+
+module UmlautThreeSixtyLink
+  # Your code goes here...
+end
---
+++ lib/umlaut-three-sixty-link/version.rb
@@ -0,0 +1,12 @@
+# Copyright (c) 2016, Regents of the University of Michigan.
+# All rights reserved. See LICENSE.txt for details.
+
+module Umlaut
+  module Three
+    module Sixty
+      module Link
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end

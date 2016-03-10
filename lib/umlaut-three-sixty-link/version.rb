4ed90a188f9539144ea0dce9844b2ce82cd30064
Albert Bertram <bertrama@umich.edu>
2016-03-10 10:32:40 -0500

    Adding in travis-ci.

--- lib/umlaut-three-sixty-link/version.rb
+++ lib/umlaut-three-sixty-link/version.rb
@@ -1,12 +1,6 @@
 # Copyright (c) 2016, Regents of the University of Michigan.
 # All rights reserved. See LICENSE.txt for details.
 
-module Umlaut
-  module Three
-    module Sixty
-      module Link
-        VERSION = "0.1.0"
-      end
-    end
-  end
+module UmlautThreeSixtyLink
+  VERSION = "0.0.1"
 end

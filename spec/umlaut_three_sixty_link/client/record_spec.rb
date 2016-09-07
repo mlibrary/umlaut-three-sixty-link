9e8b1156b6be1f56aa7d63c7d5fb7ef845ca1897
Albert Bertram <bertrama@umich.edu>
2016-09-07 14:09:20 -0400

    Removing tests that were too specific.

--- spec/umlaut_three_sixty_link/client/record_spec.rb
+++ spec/umlaut_three_sixty_link/client/record_spec.rb
@@ -7,50 +7,9 @@ RSpec.describe UmlautThreeSixtyLink::Client::Record do
     UmlautThreeSixtyLink::Client::RecordList.from_xml(xml).first
   end
 
-  let(:request) do
-    double('Request', add_service_response: nil)
-  end
-
   describe '#links' do
     it 'has links' do
       expect(record.links.length).to be(2)
-    end
-  end
-
-  describe '#add_fulltext' do
-    it 'calls request.add_service_request()' do
-      arguments = [
-        {
-          service_type_value: 'fulltext_bundle'
-        },
-        {
-          display_text: 'Cognition',
-          notes: 'Elsevier / ScienceDirect (Online service) (1972 - ...)',
-          url: 'http://proxy.lib.umich.edu/login?url=http://www.sciencedirect.com/science/journal/00100277'
-        },
-        {
-          display_text: 'Cognition',
-          notes: 'Elsevier / ScienceDirect (Online service) (1972 - ...)',
-          url: 'http://proxy.lib.umich.edu/login?url=http://www.sciencedirect.com'
-        },
-        {
-          display_text: 'Cognition',
-          notes: 'Elsevier / ScienceDirect Freedom Collection (01/01/1995 - ...)',
-          url: 'http://proxy.lib.umich.edu/login?url=http://www.sciencedirect.com/science/journal/00100277'
-        },
-        {
-          display_text: 'Cognition',
-          notes: 'Elsevier / ScienceDirect Freedom Collection (01/01/1995 - ...)',
-          url: 'http://proxy.lib.umich.edu/login?url=http://www.sciencedirect.com/'
-        }
-
-      ]
-      arguments.each do |argument|
-        expect(request)
-          .to receive(:add_service_response)
-          .with(hash_including(argument))
-      end
-      record.add_fulltext(request, {})
     end
   end
 

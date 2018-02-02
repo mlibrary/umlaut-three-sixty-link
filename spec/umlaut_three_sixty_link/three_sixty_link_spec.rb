f4d68d2386f5a160c79befc87e988170381df4d8
Bryan Hockey <bhock@umich.edu>
2018-02-02 14:17:47 -0600

    Fix broken test of ::load_config

--- spec/umlaut_three_sixty_link/three_sixty_link_spec.rb
+++ spec/umlaut_three_sixty_link/three_sixty_link_spec.rb
@@ -2,11 +2,27 @@ require 'spec_helper'
 require 'umlaut-three-sixty-link'
 
 RSpec.describe UmlautThreeSixtyLink do
+  let(:weight) do
+    {
+      "DCD" => 1,
+      "7FN" => 2,
+      "FXG" => 3,
+      "TRM" => 4,
+    }
+  end
+  let(:provider) do
+    {
+      "DCD" => "PRVBGY",
+      "7FN" => "PRVBGY",
+      "FXG" => "PRVACK",
+      "TRM" => "PRVSPB",
+    }
+  end
   describe '::load_config' do
     it 'loads a hash' do
       described_class.load_config(File.dirname(__FILE__) + '/data/360link.yml')
-      expect(described_class.weight).to eq('Weight')
-      expect(described_class.provider).to eq('Provider')
+      expect(described_class.weight).to eql(weight)
+      expect(described_class.provider).to eql(provider)
     end
   end
 end
